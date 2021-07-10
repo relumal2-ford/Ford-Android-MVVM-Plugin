108139130907dd1d549071abb0a4acbc926388c3
GitHub Action <action@github.com>
2021-07-10 08:34:06 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "Ford-Android-MVVM-Plugin"
---
+++ src/main/kotlin/com/github/relumal2ford/fordandroidmvvmplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.relumal2ford.fordandroidmvvmplugin.services
+
+import com.github.relumal2ford.fordandroidmvvmplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
